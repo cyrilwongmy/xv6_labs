5586bd0f71413b4fe0d29d9b3e78f0cc4dcf7317
scymw2 <scymw2@nottingham.edu.cn>
2021-01-25 17:34:25 +0800

    feat(kernel/file.h, kernel/fs.c, kernel/fs.h): lab fs task1: add double indirect block for xv6

--- kernel/fs.c
+++ kernel/fs.c
@@ -221,6 +221,7 @@ ialloc(uint dev, short type)
 void
 iupdate(struct inode *ip)
 {
+  
   struct buf *bp;
   struct dinode *dip;
 
@@ -400,6 +401,30 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+	bn -= NINDIRECT;
+
+	if (bn < NINDIRECT * NINDIRECT) {
+		// Load double indirect block, allocating if necessary.
+		if ((addr = ip->addrs[NDIRECT + 1]) == 0) {
+			ip->addrs[NDIRECT + 1] = addr = balloc(ip->dev);
+		}
+		bp = bread(ip->dev, addr);
+		a = (uint*)bp->data;
+		if((addr = a[bn / NINDIRECT]) == 0) {
+			a[bn / 256]	= addr = balloc(ip->dev);	
+			log_write(bp);
+		}
+		brelse(bp);
+
+		bp = bread(ip->dev, addr);
+		a = (uint*)bp->data;
+		if((addr = a[bn % NINDIRECT]) == 0) {
+			a[bn % NINDIRECT] = addr = balloc(ip->dev);
+			log_write(bp);
+		}
+		brelse(bp);
+		return addr;
+	}
 
   panic("bmap: out of range");
 }
@@ -431,6 +456,25 @@ itrunc(struct inode *ip)
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
   }
+
+	if(ip->addrs[NDIRECT + 1]){
+		bp = bread(ip->dev, ip->addrs[NDIRECT + 1]);
+		a = (uint*)bp->data;
+		for(i = 0; i < NINDIRECT; i++){
+			if(a[i]) {
+				struct buf *bp2 = bread(ip->dev, a[i]);
+				uint * b = (uint*)bp2->data;
+				for(j = 0; j < NINDIRECT; j++){
+					if(b[j])
+						bfree(ip->dev, b[j]);
+				}
+				brelse(bp2);
+			}
+		}
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT + 1]);
+    ip->addrs[NDIRECT + 1] = 0;
+	}
 
   ip->size = 0;
   iupdate(ip);
